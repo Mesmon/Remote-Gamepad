7884f2db75d6ae963fa9b269868136180ac2790a
Mesmon <mishka.ber@gmail.com>
2023-05-06 23:26:40 +0300

    Client (#1)
    * Update client code to send joystick info
    
    * Update client
    
    * BRUH
    
    * update
    
    * uppup
    
    * Fix update interval

--- client/client.py
+++ client/client.py
@@ -1,69 +1,62 @@
-import time
-import requests
 import pygame
+import requests
+import json
+import time
 from dotenv import load_dotenv
+
 load_dotenv()
 import os
 
-# Initialize pygame
+# Initialize Pygame and the joystick
 pygame.init()
+pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-# Define constants for input event types
-JOYSTICK_EVENT_TYPE = "joystick"
-QUIT_EVENT_TYPE = "quit"
+# Define the server URL
+server_url = os.environ.get("HOST_URL")
 
-# Define constants for number of events to send in a single request
-EVENTS_PER_REQUEST = 10
-EVENTS_PER_GROUP = 10
+# Define the time interval between updates in seconds
+update_interval = float(os.environ.get("UPDATE_INTERVAL"))
 
-# Set up the server endpoint
-url = os.environ.get("HOST_URL")
-
-# Set up session for reusing connection
+# Start the HTTP session
 session = requests.Session()
+# Set the headers to indicate that the payload is JSON
+headers = {"Content-type": "application/json"}
+session.headers.update(headers)
 
-# Define function to send input events to server
-def send_events(events):
-    payload = {"events": events}
-    session.post(url, json=payload)
-
-# Define function to get input events from pygame
-def get_events():
-    events = []
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            events.append({"type": QUIT_EVENT_TYPE})
-        elif event.type == pygame.JOYAXISMOTION:
-            x, y = joystick.get_axis(0), joystick.get_axis(1)
-            
-            events.append({"type": JOYSTICK_EVENT_TYPE, "x": x, "y": y})
-    return events
-
-# Main loop
+# Continuously send joystick input to the server every 20 ms
 while True:
-    # Get input events from pygame
-    events = get_events()
-    # Group joystick events and average them
-    grouped_events = []
-    num_events = len(events)
-    for i in range(0, num_events, EVENTS_PER_GROUP):
-        group = events[i:i+EVENTS_PER_GROUP]
-        joystick_events = [event for event in group if event["type"] == JOYSTICK_EVENT_TYPE]
-        if len(joystick_events) > 0:
-            x_avg = sum([event["x"] for event in joystick_events]) / len(joystick_events)
-            y_avg = sum([event["y"] for event in joystick_events]) / len(joystick_events)
-            grouped_events.append({"type": JOYSTICK_EVENT_TYPE, "x": x_avg, "y": y_avg})
-    events = [event for event in events if event["type"] != JOYSTICK_EVENT_TYPE] + grouped_events
-    
-    # Send events to server in batches of EVENTS_PER_REQUEST
-    num_events = len(events)
-    for i in range(0, num_events, EVENTS_PER_REQUEST):
-        batch = events[i:i+EVENTS_PER_REQUEST]
-        print("Sent", events[0])
-        send_events(batch)
-    
-    # Sleep for a short time to avoid overwhelming the server with requests
-    time.sleep(0.01)
+    time.sleep(update_interval)
+    pygame.event.pump()
+    joystick_stats = {}
+
+    # Axes
+    axes = []
+    for i in range(joystick.get_numaxes()):
+        axis = joystick.get_axis(i)
+        axes.append(axis)
+    joystick_stats["axes"] = axes
+
+    buttons = []
+    for i in range(joystick.get_numbuttons()):
+        button = joystick.get_button(i)
+        buttons.append(button)
+    joystick_stats["buttons"] = buttons
+
+    hats = []
+    for i in range(joystick.get_numhats()):
+        hat = joystick.get_hat(i)
+        hats.append(hat)
+    joystick_stats["hats"] = hats
+
+    print(joystick_stats["hats"])
+
+    # Convert the input data to JSON format
+    json_data = json.dumps(joystick_stats)
+    print(json_data)
+    # Send the input data as a POST request to the server
+    response = session.post(server_url, data=json_data)
+
+    # Print the response from the server
+    print(response.text)
