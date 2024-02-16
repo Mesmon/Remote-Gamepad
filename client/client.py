c0bee217b32e2c0ab5ccb5f726f040419b34a31e
Michael Berezovsky <mishka.ber@gmail.com>
2024-02-16 15:08:57 +0200

    Test

--- client/client.py
+++ client/client.py
@@ -1,11 +1,11 @@
+from websockets.sync.client import connect
+import os
 import pygame
-import requests
 import json
 import time
+from loguru import logger
 from dotenv import load_dotenv
-
 load_dotenv()
-import os
 
 # Initialize Pygame and the joystick
 pygame.init()
@@ -19,44 +19,48 @@ server_url = os.environ.get("HOST_URL")
 # Define the time interval between updates in seconds
 update_interval = float(os.environ.get("UPDATE_INTERVAL"))
 
-# Start the HTTP session
-session = requests.Session()
-# Set the headers to indicate that the payload is JSON
-headers = {"Content-type": "application/json"}
-session.headers.update(headers)
-
 # Continuously send joystick input to the server every 20 ms
-while True:
-    time.sleep(update_interval)
-    pygame.event.pump()
-    joystick_stats = {}
 
-    # Axes
-    axes = []
-    for i in range(joystick.get_numaxes()):
-        axis = joystick.get_axis(i)
-        axes.append(axis)
-    joystick_stats["axes"] = axes
 
-    buttons = []
-    for i in range(joystick.get_numbuttons()):
-        button = joystick.get_button(i)
-        buttons.append(button)
-    joystick_stats["buttons"] = buttons
+def joystick_data_message_producer(websocket):
+    while True:
+        # time.sleep(update_interval)
+        pygame.event.pump()
+        joystick_stats = {}
 
-    hats = []
-    for i in range(joystick.get_numhats()):
-        hat = joystick.get_hat(i)
-        hats.append(hat)
-    joystick_stats["hats"] = hats
+        # Axes
+        axes = []
+        for i in range(joystick.get_numaxes()):
+            axis = joystick.get_axis(i)
+            axes.append(axis)
+        joystick_stats["axes"] = axes
 
-    print(joystick_stats["hats"])
+        buttons = []
+        for i in range(joystick.get_numbuttons()):
+            button = joystick.get_button(i)
+            buttons.append(button)
+        joystick_stats["buttons"] = buttons
 
-    # Convert the input data to JSON format
-    json_data = json.dumps(joystick_stats)
-    print(json_data)
-    # Send the input data as a POST request to the server
-    response = session.post(server_url, data=json_data)
+        hats = []
+        for i in range(joystick.get_numhats()):
+            hat = joystick.get_hat(i)
+            hats.append(hat)
+        joystick_stats["hats"] = hats
 
-    # Print the response from the server
-    print(response.text)
+        logger.info(joystick_stats["hats"])
+
+        # Convert the input data to JSON format
+        json_data = json.dumps(joystick_stats)
+        # Send the input data as a POST request to the server
+        websocket.send(json_data)
+        message = websocket.recv()
+        # Print the response from the server
+        logger.info(f"Received: {message}")
+
+
+def send_joystick_data():
+    with connect("ws://localhost:8765") as websocket:
+        joystick_data_message_producer(websocket)
+
+
+send_joystick_data()
--- server/server.py
+++ server/server.py
@@ -1,37 +1,37 @@
-from flask import Flask, request
-import threading
+import asyncio
+from websockets.server import serve
 import pyvjoy
-import logging
+import json
 
-log = logging.getLogger("werkzeug")
-log.setLevel(logging.ERROR)
-
-app = Flask(__name__)
+from loguru import logger
 
 # Define constants for converting joystick input to mouse movement
 MOUSE_SPEED = 20
 JOYSTICK_SPEED = 2
+JOYSTICK_RANGE = 32767
 motion = [0, 0]
 
+vj = pyvjoy.VJoyDevice(1)
 
-@app.route("/input", methods=["POST"])
-def handle_input():
-    # Initialize virtual joystick
-    vj = pyvjoy.VJoyDevice(1)
-    data = request.json
 
+def joystick_to_vjoy(joystick_stats):
+    data = json.loads(joystick_stats)
     x_left = data["axes"][0]
     y_left = data["axes"][1]
     x_right = data["axes"][2]
     y_right = data["axes"][3]
 
-    print(int((x_left + 1) * 32767), int((y_left + 1) * 32767))
-    # Update virtual joystick axes
-    vj.set_axis(pyvjoy.HID_USAGE_X, int((x_left + 1) * 32767 / 2))
-    vj.set_axis(pyvjoy.HID_USAGE_Y, int((y_left + 1) * 32767 / 2))
+    logger.info(int((x_left + 1) * JOYSTICK_RANGE),
+                int((y_left + 1) * JOYSTICK_RANGE))
 
-    vj.set_axis(pyvjoy.HID_USAGE_RX, int((x_right + 1) * 32767 / 2))
-    vj.set_axis(pyvjoy.HID_USAGE_RY, int((y_right + 1) * 32767 / 2))
+    # Update virtual joystick axes
+    vj.set_axis(pyvjoy.HID_USAGE_X, int((x_left + 1) * JOYSTICK_RANGE / 2))
+    vj.set_axis(pyvjoy.HID_USAGE_Y, int((y_left + 1) * JOYSTICK_RANGE / 2))
+
+    vj.set_axis(pyvjoy.HID_USAGE_RX, int(
+        (x_right + 1) * JOYSTICK_RANGE / 2))
+    vj.set_axis(pyvjoy.HID_USAGE_RY, int(
+        (y_right + 1) * JOYSTICK_RANGE / 2))
 
     buttons = data["buttons"]
     for button_index in range(len(buttons)):
@@ -51,24 +51,17 @@ def handle_input():
     }
     vj.set_cont_pov(1, pov_hat_map[tuple(hats[0])])
 
-    # events = data["events"]
-    # # Iterate over input events and process each one
-    # for event in events:
-    #     if event["type"] == "joystick":
-    #         x = event["x"]
-    #         y = event["y"]
 
-    #         # Update virtual joystick axes
-    #         j.set_axis(pyvjoy.HID_USAGE_X, int((x + 1) * 32767))
-    #         j.set_axis(pyvjoy.HID_USAGE_Y, int((y + 1) * 32767))
-
-    #     elif event["type"] == "quit":
-    #         j.reset()
-    #         j.close()
-    #         # Quit the application
-    #         return "Application terminated."
-    return "Input received."
+async def handle_joystick(websocket):
+    async for message in websocket:
+        logger.info(f"Received: {message}")
+        joystick_to_vjoy(message)
+        await websocket.send("Received")
+        logger.info("Sent: Received")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+async def main():
+    async with serve(handle_joystick, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
